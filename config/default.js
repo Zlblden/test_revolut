1569316d19d93134b9e1ec3c2a11b3c097cecb63
Dmitriy Balakhonov <sosikita403@yandex.ru>
2020-03-24 14:17:45 +0300

    Test revolut

---
+++ config/default.js
@@ -0,0 +1,6 @@
+module.exports = {
+    authorizationURL: 'business.revolut.com/app-confirm',
+    clientID: 'vR3QIcN6lCJ6cjRcH-NOoqfzqJjWuElthYUcBbyDSBI',
+    redirect_uri: 'https://sandbox-business.revolut.com/',
+    pathToPem: '/keys/privatekey.pem'
+};
